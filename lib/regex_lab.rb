148de7b2f4a8bcb86758c94fb2789786748a3de9
felixNyalenda <felixasejunior@gmail.com>
2023-05-18 11:06:36 +0300

    first 3

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,27 @@
 def starts_with_a_vowel?(word)
+  return true if word.match(/\A[aeiouAEIOU]/)
 
+  false
 end
+
+# print starts_with_a_vowel? 'Felix'
+
+un_ing = 'unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary'
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w*ing\b/)
 end
+
+# print words_starting_with_un_and_ending_with_ing un_ing
+
+words_string = 'extreme briny crepe parking snaps grouping snafu round dog be fork spoon'
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
+print words_five_letters_long words_string
 
-end
+def first_word_capitalized_and_ends_with_punctuation?(text); end
 
-def valid_phone_number?(phone)
-
-end
+def valid_phone_number?(phone); end
